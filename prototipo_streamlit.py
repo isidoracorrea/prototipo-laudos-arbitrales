ca9ca44fb7b61dff0f440f349b2737c2e308d6ce
isidoracorrea <misidora6@gmail.com>
2025-05-24 20:05:05 -0400

    Update prototipo_streamlit.py

--- prototipo_streamlit.py
+++ prototipo_streamlit.py
@@ -8,12 +8,9 @@ df = pd.read_csv("dataset_prototipo_streamlit.csv")
 st.title("Prototipo de Consulta de Causales - Recursos de Anulación")
 
 # Descripción breve
-st.markdown("""
-st.markdown("""
-Esta herramienta permite consultar de forma rápida los patrones identificados en los recursos de anulación de laudos arbitrales en Lima (2023).
+st.markdown("""Esta herramienta permite consultar de forma rápida los patrones identificados en los recursos de anulación de laudos arbitrales en Lima (2023).
 Las causales que se muestran corresponden al **artículo 63 del Decreto Legislativo Nro. 1071**, que regula las causales específicas por las que se puede solicitar la nulidad de un laudo arbitral en Perú.
 Selecciona una causal para ver su frecuencia, probabilidad de acogida o rechazo, argumentos comunes y fundamentos del tribunal.
-""")
 """)
 
 # Selector de causal
